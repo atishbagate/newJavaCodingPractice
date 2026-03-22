a6791e46f8d4cb71c141c8c03d8d221d06b0b5e9
Atish <bagateatish@gmail.com>
2026-03-22 17:21:50 +0530

    observer pattern added.
    Signed-off-by: Atish <bagateatish@gmail.com>

--- src/DesignPatterns/BehavioralPatterns/ObserverPattern/ObserverPatternExampleOne.java
+++ src/DesignPatterns/BehavioralPatterns/ObserverPattern/ObserverPatternExampleOne.java
@@ -3,23 +3,27 @@ package DesignPatterns.BehavioralPatterns.ObserverPattern;
 import java.util.ArrayList;
 import java.util.List;
 
-interface Observer{
+interface Observer {
     void update(float temp);
 }
-interface Subject{
+
+interface Subject {
     void attach(Observer obs);
+
     void detach(Observer obs);
+
     void notifyObservers();
 }
 
-class  WeatherStation implements Subject{
+class WeatherStation implements Subject {
     private float temperature;
     private List<Observer> observerList;
 
-    public WeatherStation(){
+    public WeatherStation() {
         this.observerList = new ArrayList<>();
     }
-    public void setTemperature(float temp){
+
+    public void setTemperature(float temp) {
         this.temperature = temp;
         notifyObservers();
     }
@@ -31,31 +35,31 @@ class  WeatherStation implements Subject{
 
     @Override
     public void detach(Observer obs) {
-        System.out.println("detaching "+obs);
-    observerList.remove(obs);
+        System.out.println("detaching " + obs);
+        observerList.remove(obs);
     }
 
     @Override
     public void notifyObservers() {
-        for (Observer obs : observerList){
+        for (Observer obs : observerList) {
             obs.update(temperature); // run time polymorphism
         }
     }
 }
 
-class DisplayDevice implements Observer{
+class DisplayDevice implements Observer {
     @Override
     public void update(float temp) {
-        System.out.println(" temp in display device "+temp);
+        System.out.println(" temp in display device " + temp);
     }
 
 }
 
-class MobileDevice implements Observer{
+class MobileDevice implements Observer {
 
     @Override
     public void update(float temp) {
-        System.out.println(" temp in mobile device "+temp);
+        System.out.println(" temp in mobile device " + temp);
     }
 }
 
@@ -76,6 +80,6 @@ public class ObserverPatternExampleOne {
         weatherStation.setTemperature(23);
 
 //        detaching device
-         weatherStation.detach(displayDevice);
+        weatherStation.detach(displayDevice);
     }
 }
